bd337721aa57b2a38b258321f74a86c2a9214c2d
Chris F <abigdeer@hotmail.com>
2016-10-13 01:56:08 +1100

    Add GradleTrackerConsole

---
+++ src/main/java/console/GradeTrackerConsole.java
@@ -0,0 +1,48 @@
+package console;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class GradeTrackerConsole {
+    public static void main(String[] args) {
+
+        String input;
+        int weighting;
+        double mark;
+        int total;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("** -- Grade Tracker Console -- **");
+
+        //while (true) {
+            try {
+                System.out.println("Enter in assignment weighting (numeric): ");
+                input = br.readLine();
+                weighting = Integer.parseInt(input);
+
+                System.out.println("Enter in mark: ");
+                input = br.readLine();
+                mark = Double.parseDouble(input);
+
+                System.out.println("What was the mark out of? ");
+                input = br.readLine();
+                total = Integer.parseInt(input);
+
+                calculateValues(weighting, mark, total);
+
+                //break;
+
+            }   catch (Exception e) {
+                e.printStackTrace();
+            }
+        //}
+
+
+    }
+
+    private static void calculateValues(int weight, double mark, int total) {
+        System.out.println("Total percentage of assignment: " + (mark/total * 100) + "/100");
+        System.out.println("Total percentage in unit (weighted): " + (mark/total * weight) + "/100");
+    }
+}
