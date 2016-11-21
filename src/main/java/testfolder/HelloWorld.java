1ccb86b221f3d15dfb3c17d27c43f4a2d20bf1fb
Barkley R <barkley_reynolds@hotmail.com>
2016-11-21 22:45:06 +1100

    merge test

--- src/main/java/testfolder/HelloWorld.java
+++ src/main/java/testfolder/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 
         LocalTime currentTime = new LocalTime();
         System.out.println("The current local time is: " + currentTime);
-        //comment
+        System.out.println("give me an error");
         //sand nigger love
         System.out.println("Hello world!");
         System.out.println("Jono is love0");
