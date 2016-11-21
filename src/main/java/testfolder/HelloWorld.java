d585eb349f457f04287a2aacd14af19f11f1c64b
Barkley R <barkley_reynolds@hotmail.com>
2016-11-21 22:24:45 +1100

    [little bit of jono in my life]

--- src/main/java/testfolder/HelloWorld.java
+++ src/main/java/testfolder/HelloWorld.java
@@ -9,5 +9,7 @@ public class HelloWorld {
         System.out.println("The current local time is: " + currentTime);
 
         System.out.println("Hello world!");
+        System.out.println("Jono is love0");
+
     }
 }
